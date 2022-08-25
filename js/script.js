06aec9337ac62017ad54e3e72adbe8a16c383f80
Denis-92 <d.mozenic@gmail.com>
2022-08-25 16:05:03 +0200

    aggiunto condizione "Buzz"

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ console.log('JS - OK');
 for (i = 1; i <= 100; i++) {
     if (i % 3 === 0)
         console.log('Fizz');
-    else
+    if (i % 5 === 0)
+        console.log('Buzz');
+    if (i % 3 !== 0 && i % 5 !== 0)
         console.log(i);
 }
