a244e2bb53616d0186c043f915e75ac6ce839efc
Denis-92 <d.mozenic@gmail.com>
2022-08-25 16:32:59 +0200

    controllo con log

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 console.log('JS - OK');
 
+console.log('ciclo for inizio');
 for (i = 1; i <= 100; i++) {
 
 
     if (i % 3 !== 0 && i % 5 !== 0) { console.log(i); }
     else {
-        if (i % 3 === 0 && i % 5 === 0) { console.log('FizzBuzz'); }
+        if (i % 3 === 0 && i % 5 === 0) { console.log('FizzBuzz' + i); }
         else {
-            if (i % 3 === 0) { console.log('Fizz'); }
-            if (i % 5 === 0) { console.log('Buzz'); }
+            if (i % 3 === 0) { console.log('Fizz' + i); }
+            if (i % 5 === 0) { console.log('Buzz' + i); }
         }
     }
 }
+console.log('ciclo for fine');
+
+console.log('JS - END');
